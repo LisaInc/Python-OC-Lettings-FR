b8beeff2f1a9247b73db8587d8b57bb1fa4b930b
LisaIncardona <lincardona@birds-company.com>
2022-09-30 16:51:51 +0200

    add lettings and profile app and migrate data

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from .views import index
+from lettings.views import index as lettings_index, letting
+from profiles.views import index as profiles_index, profile
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
-    path('admin/', admin.site.urls),
+    path("", index, name="index"),
+    path("lettings/", lettings_index, name="lettings_index"),
+    path("lettings/<int:letting_id>/", letting, name="letting"),
+    path("profiles/", profiles_index, name="profiles_index"),
+    path("profiles/<str:username>/", profile, name="profile"),
+    path("admin/", admin.site.urls),
 ]
